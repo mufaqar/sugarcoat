75f5a089219e6487628cf2828e7b9b3117c2c537
syed zunair gillani <gillanix007@gmail.com>
2023-05-31 14:45:14 +0500

    ClassName

--- src/app/page.js
+++ src/app/page.js
@@ -3,26 +3,26 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div class="">
+      <div className="">
         {/* main section */}
-        <main class="w-full h-screen bg-[url('/images/11.png')] bg-no-repeat bg-cover border-b-[1px] border-gray-200">
-          <div class="text-center text-white flex flex-col justify-end items-center h-screen px-4">
-            <h1 class="text-6xl max-sm:text-xl font-light sm:tracking-[3vh] tracking-[3vh] pb-4 ">
+        <main className="w-full h-screen bg-[url('/images/11.png')] bg-no-repeat bg-cover border-b-[1px] border-gray-200">
+          <div className="text-center text-white flex flex-col justify-end items-center h-screen px-4">
+            <h1 className="text-6xl max-sm:text-xl font-light sm:tracking-[3vh] tracking-[3vh] pb-4 ">
               SUGARCOAT
             </h1>
-            <p class="mb-6 font-light ">WE MAKE LIFE SWEETER</p>
-            <a href="#" class="px-10 mb-[1px] py-3 text-black bg-white ">
+            <p className="mb-6 font-light ">WE MAKE LIFE SWEETER</p>
+            <a href="#" className="px-10 mb-[1px] py-3 text-black bg-white ">
               BOOK AN APPOINTENT
             </a>
           </div>
         </main>
 
-        <section class="px-4 mt-40 ">
-          <div class="grid grid-cols-2 px-12 md:px-20 max-sm:grid-cols-1 border-2 border-black mx-auto py-20 container ">
-            <div class="mt-10 text-5xl font-light">
+        <section className="px-4 mt-40 ">
+          <div className="grid grid-cols-2 px-12 md:px-20 max-sm:grid-cols-1 border-2 border-black mx-auto py-20 container ">
+            <div className="mt-10 text-5xl font-light">
               <h2 className="font-kammerlander">OUR VISION</h2>
             </div>
-            <div class="mt-5">
+            <div className="mt-5">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
                 placeat, ab sit quidem quod perferendis perspiciatis, at eos,
@@ -34,14 +34,14 @@ export default function Home() {
           </div>
         </section>
 
-        <div class="mt-40 text-5xl font-light text-center container mx-auto">
+        <div className="mt-40 text-5xl font-light text-center container mx-auto">
           <h2 className="font-kammerlander">OUR SERVICE</h2>
         </div>
-        <div class="grid grid-cols-1 gap-10 px-10 mt-20 sm:grid-cols-2 lg:grid-cols-3 container mx-auto">
+        <div className="grid grid-cols-1 gap-10 px-10 mt-20 sm:grid-cols-2 lg:grid-cols-3 container mx-auto">
           {[1, 2, 3, 4, 5, 6].map((item, idx) => {
             return (
               <div
-                class="w-full p-5 bg-white border border-black"
+                className="w-full p-5 bg-white border border-black"
                 key={idx}
               >
                 <div>
@@ -51,9 +51,9 @@ export default function Home() {
                     className="border border-black"
                   />
                 </div>
-                <div class="mt-4 text-center">
+                <div className="mt-4 text-center">
                   <h5 className="font-boxley font-light text-2xl">NAIL CARE</h5>
-                  <h3 class="mt-5 text-2xl lg:text-4xl font-semibold font-kammerlander">
+                  <h3 className="mt-5 text-2xl lg:text-4xl font-semibold font-kammerlander">
                     MANICURES + PEDICURES
                   </h3>
                 </div>
@@ -62,43 +62,43 @@ export default function Home() {
           })}
         </div>
 
-        <section class="flex flex-col md:flex-row gap-5 px-10 mt-40 container mx-auto">
+        <section className="flex flex-col md:flex-row gap-5 px-10 mt-40 container mx-auto">
           <div className="py-5 lg:max-w-[35%] w-full">
-          <div class="w-full p-4 border h-full border-black sm:p-6 md:p-8 flex flex-col justify-center items-center">
-            <div class="text-4xl font-light text-center max-sm:text-lg">
+          <div className="w-full p-4 border h-full border-black sm:p-6 md:p-8 flex flex-col justify-center items-center">
+            <div className="text-4xl font-light text-center max-sm:text-lg">
               <h2 className="font-kammerlander">
                 SUGARCOAT <br /> NAIL POLISHES
               </h2>
             </div>
-            <div class="mt-10 flex flex-col justify-center gap-2 items-center text-center">
+            <div className="mt-10 flex flex-col justify-center gap-2 items-center text-center">
               <Image src="/svg/leaf.svg" alt="leaf" width={120} height={120} className="w-12 h-12 border border-black rounded-full p-2"/>
               <h3 className="font-boxley ">ORGANIC</h3>
             </div>
-            <div class="mt-10 flex flex-col justify-center gap-2 items-center text-center">
+            <div className="mt-10 flex flex-col justify-center gap-2 items-center text-center">
               <Image src="/svg/wave.svg" alt="leaf" width={120} height={120} className="w-12 h-12 border border-black rounded-full p-2"/>
               <h3 className="font-boxley ">NON-TOXIC</h3>
             </div>
-            <div class="mt-10 flex flex-col justify-center gap-2 items-center text-center">
+            <div className="mt-10 flex flex-col justify-center gap-2 items-center text-center">
               <Image src="/svg/eco.svg" alt="leaf" width={120} height={120} className="w-12 h-12 border border-black rounded-full p-2"/>
               <h3 className="font-boxley ">ECO-FRIENDLY</h3>
             </div>
           </div>
           </div>
           <div className="w-full h-full max-h-[750px]">
-            <img src="/images/nailpolish.png" alt="" class="w-full h-full" />
+            <img src="/images/nailpolish.png" alt="" className="w-full h-full" />
           </div>
         </section>
 
-        <div class="mt-40 text-5xl text-center ">
+        <div className="mt-40 text-5xl text-center ">
           <h2 className="font-kammerlander">
             CLIENT <br /> TESTIMONIALS
           </h2>
         </div>
 
-        <div class="grid grid-cols-3 gap-20 px-10 max-sm:grid-cols-1 max-md:grid-cols-2 container mx-auto">
-          <div class="mt-20 text-center ">
-            <hr class="border-black" />
-            <p class="mt-10">
+        <div className="grid grid-cols-3 gap-20 px-10 max-sm:grid-cols-1 max-md:grid-cols-2 container mx-auto">
+          <div className="mt-20 text-center ">
+            <hr className="border-black" />
+            <p className="mt-10">
               "I LOVE GOING TO SUGARCOAT IN ATLANTA-THEIR MENU IS PERFECT. IF
               I'M ON THE GO, I'LL POP IN FOR A QUICK EXPRESS MANICURE. ON DAYS
               WHEN I HAVE TIME TO RELAX, I'LL PAMPER MYSELF WITH THEIR
@@ -106,15 +106,15 @@ export default function Home() {
               SCRUB. PLUS, I LOVE THE PINK AND WHITE DECOR AT THE BUCKHEAD
               LOCATION. IT'S VERY GIRLIE AND REMINDS ME OF THE SPANX OFFICES! "
             </p>
-            <h4 class="mt-10 text-2xl font-kammerlander">
+            <h4 className="mt-10 text-2xl font-kammerlander">
               SARA BLAKELY
               <br />
               FOUNDER OF SPANX ❤
             </h4>
           </div>
-          <div class="mt-20 text-center">
-            <hr class="border-black" />
-            <p class="mt-10">
+          <div className="mt-20 text-center">
+            <hr className="border-black" />
+            <p className="mt-10">
               "BOOST YOUR PRODUCT AND SERVICE'S CREDIBILITY BY ADDING
               TESTIMONIALS FROM YOUR CLIENTS.
               <br />
@@ -123,20 +123,20 @@ export default function Home() {
               PEOPLE LOVE RECOMMENDATIONS SO FEEDBACK FROM OTHERS WHO'VE TRIED
               IT IS INVALUABLE."
             </p>
-            <h4 class="mt-10 text-2xl font-kammerlander">
+            <h4 className="mt-10 text-2xl font-kammerlander">
               ERICA BOGART
               <br />
               AWARD WINNING MAKEUP ARTIST
             </h4>
           </div>
-          <div class="mt-20 text-center">
-            <hr class="border-black" />
-            <p class="mt-10 font-boxley">
+          <div className="mt-20 text-center">
+            <hr className="border-black" />
+            <p className="mt-10 font-boxley">
               "BOOST YOUR PRODUCT AND SERVICE'S CREDIBILITY BY ADDING
               TESTIMONIALS FROM YOUR CLIENTS. FEEDBACK FROM OTHERS WHO'VE TRIED
               IT IS INVALUABLE. "
             </p>
-            <h4 class="mt-5 text-2xl font-kammerlander">
+            <h4 className="mt-5 text-2xl font-kammerlander">
               BRENDA WOOD
               <br />
               NEWS ANCHOR
@@ -144,13 +144,13 @@ export default function Home() {
           </div>
         </div>
 
-        <div class="w-full py-20 mt-20 bg-black">
-          <div class="pt-10 text-center text-white">
-            <h5 class="font-semibold font-boxley">ABOUT THE BRAND</h5>
-            <h2 class="mt-20 font-light sm:text-3xl text-2xl md:text-6x xl:text-8xl font-kammerlander">
+        <div className="w-full py-20 mt-20 bg-black">
+          <div className="pt-10 text-center text-white">
+            <h5 className="font-semibold font-boxley">ABOUT THE BRAND</h5>
+            <h2 className="mt-20 font-light sm:text-3xl text-2xl md:text-6x xl:text-8xl font-kammerlander">
               WE TAKE CARE OF <br /> OUR CLIENTS AND <br /> OUR PEOPLE.
             </h2>
-            <p class="mt-8 font-boxley">
+            <p className="mt-8 font-boxley">
               WE LOVE SERVING OUR COMMUNITY, WE STRIVE TO PROVIDE THE BEST
               <br />
               PRODUCTS AND SERVICES TO CREATE THE ULTIMATE BEAUTY OASIS FOR ALL
@@ -159,23 +159,23 @@ export default function Home() {
           </div>
         </div>
 
-        <div class="w-full flex justify-center flex-col px-4 items-end py-20 bg-[url('/images/office.jpg')] bg-no-repeat bg-cover">
-          <div class= "p-4 max-w-[400px] bg-white m-auto md:mr-20">
-            <div class="border border-black p-6">
-              <div class="mt-10 text-center">
-                <h2 class="text-4xl font-kammerlander">CONTACT US</h2>
-                <h5 class="mt-10 font-bold font-boxley text-lg">PHONE</h5>
+        <div className="w-full flex justify-center flex-col px-4 items-end py-20 bg-[url('/images/office.jpg')] bg-no-repeat bg-cover">
+          <div className= "p-4 max-w-[400px] bg-white m-auto md:mr-20">
+            <div className="border border-black p-6">
+              <div className="mt-10 text-center">
+                <h2 className="text-4xl font-kammerlander">CONTACT US</h2>
+                <h5 className="mt-10 font-bold font-boxley text-lg">PHONE</h5>
                 <p className="font-boxley text-sm">(404)00080000</p>
-                <div class="mt-10">
-                  <h3 class="font-bold font-boxley text-lg">EMAIL</h3>
+                <div className="mt-10">
+                  <h3 className="font-bold font-boxley text-lg">EMAIL</h3>
                   <p className="font-boxley text-sm">HELLO@ SUGARCOATBEAUTY.COOM</p>
-                  <h3 class="mt-10 font-bold font-boxley text-lg">SOCIAL</h3>
-                  <div class="mt-2 text-xl flex justify-center item gap-3">
+                  <h3 className="mt-10 font-bold font-boxley text-lg">SOCIAL</h3>
+                  <div className="mt-2 text-xl flex justify-center item gap-3">
                   <Image src="/svg/faceboook.svg" alt="facebook" width={120} height={120} className="w-6 h-6"/>
                   <Image src="/svg/twitter.svg" alt="twitter" width={120} height={120} className="w-6 h-6"/>
                   <Image src="/svg/instagram.svg" alt="insta" width={120} height={120} className="w-6 h-6"/>
                   </div>
-                  <div class="mt-20">
+                  <div className="mt-20">
                     <p className="font-boxley">
                       OUR SALONS ARE FULLY ACCESSIBLE. PLEASE REACH OUT FOR
                       FURTHER ACCOMMODATIONS.
@@ -187,7 +187,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div class="grid grid-cols-5 grid-rows-2 gap-5 container mx-auto px-10 mt-20 logo max-sm:grid-cols-3 max-md:grid-cols-4">
+        <div className="grid grid-cols-5 grid-rows-2 gap-5 container mx-auto px-10 mt-20 logo max-sm:grid-cols-3 max-md:grid-cols-4">
           <figure className="flex justify-center items-center"><img src="/images/logo 1.jpeg" alt="" /></figure>
           <figure className="flex justify-center items-center"><img src="/images/logo2.png" alt="" /></figure>
           <figure className="flex justify-center items-center"><img src="/images/logo3.png" alt="" /></figure>
@@ -200,7 +200,7 @@ export default function Home() {
           <figure className="flex justify-center items-center"><img src="/images/logo6.jpeg" alt="" /></figure>
         </div>
 
-        <div class="w-full h-screen mt-20 bg-[url('/images/11.png')] bg-no-repeat bg-cover "/>
+        <div className="w-full h-screen mt-20 bg-[url('/images/11.png')] bg-no-repeat bg-cover "/>
       </div>
     </>
   );
