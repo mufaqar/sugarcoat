3ddff4ce1659f8427d6d6f9d45b96eb7a0d64fbf
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-06-05 15:29:40 +0500

    all Pages updated

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,13 +1,18 @@
 import Footer from '../components/footer/page';
 import Header from '../components/header/page';
+import Banner from '../components/main/banner';
 
 export default function Events() {
   // const [OpenNav, setOpenNav] = useState(false);
   return (
     <>
       <Header />
-
-      <section className="px-4 mt-40 " id="vision">
+      <Banner
+        image="/images/11.png"
+        title="SUGARCOAT"
+        sub_title="WE MAKE LIFE SWEETER"
+      />
+      <section className="px-4 my-20 " id="vision">
         <div className="flex items-center flex-col md:flex-row px-12 md:px-20 border py-28 gap-12 md:gap-32 border-black mx-auto container">
           <h2 className="font-kammerlander text-5xl font-light md:max-w-[400px] w-full">
             Events
