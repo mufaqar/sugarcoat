b1c34171baf51eec63ca4f6c451980354f134a5b
mufaqar <mufaqar@gmail.com>
2023-06-05 11:54:17 +0500

    Locations

---
+++ src/app/locations/page.js
@@ -0,0 +1,21 @@
+import Footer from '../components/footer/page';
+import Header from '../components/header/page';
+
+export default function Locations() {
+  // const [OpenNav, setOpenNav] = useState(false);
+  return (
+    <>
+      <Header />
+
+      <section className="px-4 mt-40 " id="vision">
+        <div className="flex items-center flex-col md:flex-row px-12 md:px-20 border py-28 gap-12 md:gap-32 border-black mx-auto container">
+          <h2 className="font-kammerlander text-5xl font-light md:max-w-[400px] w-full">
+            Locatinos
+          </h2>
+          <p className="font-boxley">Event Laout Page</p>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+}
