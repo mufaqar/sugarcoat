f13a97e92e8626b635af2f264fcfaece2d215159
syed zunair gillani <gillanix007@gmail.com>
2023-06-12 18:31:13 +0500

    update

--- public/locations.jsx
+++ public/locations.jsx
@@ -3,63 +3,63 @@ export const Locations_data = [
           name: 'Buckhead – The Shops at Pharr',
           address: '320 Pharr Road NE, Atlanta, GA 30305',
           image: '/images/locations.png',
-          num: '404-814-2121',
+          num: '404 - 814 - 2121',
           link: 'https://go.booker.com/location/sugarcoatbuckhead/'
      },
      {
           name: 'C Hastain',
           address: '4365 Roswell Rd. Suite 4385, Atlanta, GA 30342',
           image: '/images/location1.png',
-          num: '404-600-8600',
+          num: '404 - 600 - 8600',
           link: 'https://go.booker.com/location/sugarcoatChastain'
      },
      {
           name: 'Virginia Highland',
           address: '1062 St. Charles Ave. NE Atlanta, GA 30306',
           image: '/images/location2.png',
-          num: '404-249-0013',
+          num: '404 - 249 - 0013',
           link: 'https://go.booker.com/location/sugarcoatvirginiahighland'
      },
      {
           name: 'Vinings',
           address: '4300 Paces Ferry Rd. Ste 230, Atlanta, GA 30339',
           image: '/images/location3.png',
-          num: '770-801-9888',
+          num: '770 - 801 - 9888',
           link: 'https://go.booker.com/location/sugarcoatvinings'
      },
      {
           name: 'Midtown',
           address: '60 11th Street Suite 3A, Atlanta, GA 30309',
           image: '/images/location4.png',
-          num: '404-999-8688',
+          num: '404 - 999 - 8688',
           link: 'https://go.booker.com/location/sugarcoatmidtown/service-menu'
      },
      {
           name: 'Sugarcoat Lash Bar',
           address: '256 Pharr Rd. Atlanta, GA 30305',
           image: '/images/location5.png',
-          num: '404-228-4955',
+          num: '404 - 228 - 4955',
           link: '#'
      },
      {
           name: 'Phipps Plaza',
           address: '3500 Peachtree Rd. NE #2061C, Atlanta, GA 30306',
           image: '/images/location6.png',
-          num: '404-855-5507',
+          num: '404 - 855 - 5507',
           link: 'https://go.booker.com/location/SugarcoatPhippsPlaza/service-menu'
      },
      {
           name: 'Lenox Square Mall',
           address: '3393 Peachtree Rd. NE Suite 5025C, Atlanta, GA 30326',
           image: '/images/location7.png',
-          num: '404-600-8600',
+          num: '404 - 600 - 8600',
           link: 'https://go.booker.com/location/SugarcoatLenox'
      },
      {
           name: 'Hair Bar by Sugarcoat',
           address: '260 Pharr Rd. NE Suite #5, Atlanta, GA 30305',
           image: '/images/location8.png',
-          num: '678-785-4256',
+          num: '678 - 785 - 4256',
           link: 'https://go.booker.com/location/hairbarbysugarcoat'
      },
 ]
