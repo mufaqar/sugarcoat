f091f4e4cf87aa50e36595680619b239d66d1c18
mufaqar <mufaqar@gmail.com>
2023-06-23 14:22:28 +0500

    updated

--- src/app/components/service-box/service.js
+++ src/app/components/service-box/service.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-export default function ServiceBox({post}) {
-    return (
-        <div className="w-full p-5 bg-white border border-black" >
-            <div className='border border-black overflow-hidden'>
-                <img
-                    src={post?.featuredImage?.node?.mediaItemUrl}
-                    alt={post?.name}
-                    className=" w-full h-full object-cover scale-[1.02]"
-                />
-            </div>
-            <div className="mt-4 text-center">
-                <h5 className="font-montserrat font-light text-sm">{post?.title}</h5>
-                <div className="mt-5 text-3xl font-semibold font-kammerlander text-gray-600">{post?.servicesFields?.info}</div>
-            </div>
+export default function ServiceBox({ post }) {
+  return (
+    <div className="w-full p-5 bg-white border border-black">
+      <div className="border border-black overflow-hidden">
+        <img
+          src={post?.featuredImage?.node?.mediaItemUrl}
+          alt={post?.name}
+          className=" w-full h-full object-cover scale-[1.02]"
+        />
+      </div>
+      <div className="mt-4 text-center">
+        <h5 className="font-montserrat font-medium text-sm">{post?.title}</h5>
+        <div className="mt-5 text-3xl font-semibold font-kammerlander text-gray-600">
+          {post?.servicesFields?.info}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
